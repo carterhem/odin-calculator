aaf4998f92c889c5eb0a1043a1fba9d537e7eb29
Carter <carter.hemington@gmail.com>
2023-02-13 14:39:01 -0500

    add functionality to display numbers onscreen, however not tested

--- functions.js
+++ functions.js
@@ -21,10 +21,48 @@ const operate = function(a,b,operator) {
     return add(a,b);
   } else if(operator === "-") {
     return subtract(a,b);
-  } else if(operator === "*") {
+  } else if(operator === "x") {
     return multiply(a,b);
   } else if(operator === "÷") {
     //this will need to be changed to divide symbol ÷ somewhere along the way
     return divide(a,b);
   }
 }
+
+const box = document.getElementsByClassName('box')[0];
+//getElementsByClassName returns an array, so adding the [0] to make sure we have just the first item of that array
+
+const display = document.getElementsByClassName('display')[0];
+//capturing display so that I can send buttons clicked to it
+
+
+box.addEventListener('click', event => {
+  let displayValue = '';
+  if(event.target.value != 'clear' && event.target.value != "=") {
+    console.log('if')
+    display.innerHTML += event.target.value;
+    //add displayValue
+    console.log("event", event)
+    console.log("event.target", event.target)
+    console.log("event.target.value", event.target.value)
+  } else if (event.target.value == 'clear'){
+    console.log('else if clear')
+    display.innerHTML = '0';
+    //add displayValue
+    console.log("event", event)
+    console.log("event.target", event.target)
+    console.log("event.target.value", event.target.value)
+  } else if (event.target.value == "=") {
+    console.log('else if equal')
+    //somehow call the relevant functions in a loop here
+    display.innerHTML = 'sum';
+    //add displayValue
+    console.log("event", event)
+    console.log("event.target", event.target)
+    console.log("event.target.value", event.target.value)
+  }
+})
+
+//todo its something to do with append child to node, or textcontent
+//todo also have to figure out how to clear the screen the first time, but not every time a new number is entered
+//todo figure out how to keep screen from collapsing if I have no value input
