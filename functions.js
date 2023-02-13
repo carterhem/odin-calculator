a75131425d48ec8409a6dabcda6e0c5525e68cf0
Carter <carter.hemington@gmail.com>
2023-02-13 09:10:11 -0500

    create operate function and test

--- functions.js
+++ functions.js
@@ -1,18 +1,29 @@
-const add = function(a,b) {
+const add = function (a, b) {
   return a + b;
 };
 
-const subtract = function(a,b) {
+const subtract = function (a, b) {
   return a - b;
 };
 
-const multiply = function(input) {
-  //have to start at one here so we don't keep multiplying items by zero
-  return input.reduce((partialSum, a) => partialSum * a, 1);
+const multiply = function (a ,b) {
+  return a * b;
 };
 
-const divide = function(a,b) {
+const divide = function (a, b) {
   //maybe flip format to reduce like multiply, not sure why we are doing it there
   return a / b;
 };
 
+const operate = function(a,b,operator) {
+  //could change this into switch cases down the line
+  if(operator === '+') {
+    return add(a,b);
+  } else if(operator === "-") {
+    return subtract(a,b);
+  } else if(operator === "*") {
+    return multiply(a,b);
+  } else if(operator === "/") {
+    return divide(a,b);
+  }
+}
